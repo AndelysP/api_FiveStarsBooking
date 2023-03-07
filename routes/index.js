c58ee42ee7077a0be6ffec328f7be6366d5f28f6
AndelysP <andelyst@hotmail.fr>
2023-03-07 13:14:36 +0100

    :lipstick: Ajout du style pour l'e-mail de demande de contact

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,29 @@ module.exports = server => {
         const mailOptions = {
             from: `${req.body.email}`,
             to: 'hewecob405@fenwazi.com',
-            subject: 'Contact form test',
+            subject: 'Réception d\'une de demande de contact',
             html: `
-                <p>Name: ${req.body.name}</p>
-                <p>Email: ${req.body.email}</p>
-                <p>Téléphone: ${req.body.phone}</p>
-                <p>Message: ${req.body.message}</p>
+            <div style="width: 100%; background-color: #f3f9ff; padding: 5rem 0">
+            <div style="max-width: 700px; background-color: white; margin: 0 auto">
+            <div style="width: 100%; background-color: #1B212B; padding: 20px 0">
+                <img
+                    src="https://www.zupimages.net/up/23/10/yk9e.png"
+                    style="width: 100%; height: 70px; object-fit: contain"
+                />          
+            </div>
+            <div style="width: 100%; gap: 10px; padding: 30px 0; display: grid">
+            <p style="font-weight: 800; font-size: 1.2rem; padding: 0 30px">
+              Five Star's Booking
+            </p>
+            <div style="font-size: .8rem; margin: 0 30px">
+              <p>Nom / Prénom: <b>${req.body.name}</b></p>
+              <p>E-mail: <b>${req.body.email}</b></p>
+              <p>Téléphone: <b>${req.body.phone}</b></p>
+              <p>Message: <i>${req.body.message}</i></p>
+            </div>
+            </div>
+            </div>
+            </div>
             `
         };
 
