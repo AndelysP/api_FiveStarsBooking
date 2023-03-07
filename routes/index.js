afd895180eae9b5c5d8910265cb7c6ae1b98a87c
AndelysP <andelyst@hotmail.fr>
2023-03-07 11:35:03 +0100

    :rocket: Ajout de nodemailer pour récupérer les informations du formulaire de contact

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const ShipController = require("../controllers/ship");
+const nodemailer = require("nodemailer");
 
 module.exports = server => {
     server.get("/ships", (req, res) => {
@@ -7,6 +8,39 @@ module.exports = server => {
 
     server.get("/ships/:id", (req, res) => {
         ShipController.get(req, res);
+    });
+
+    server.post('/contact', function (req, res) {
+        const transporter = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "42e506b5471a1d",
+                pass: "d7bc80cb0b7fe4"
+            }
+        });
+
+        const mailOptions = {
+            from: `${req.body.email}`,
+            to: 'hewecob405@fenwazi.com',
+            subject: 'Contact form test',
+            html: `
+                <p>Name: ${req.body.name}</p>
+                <p>Email: ${req.body.email}</p>
+                <p>Téléphone: ${req.body.phone}</p>
+                <p>Message: ${req.body.message}</p>
+            `
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                res.status(500).send('Erreur lors de l\'envoie du message');
+            } else {
+                console.log('Email sent: ' + info.response);
+                res.status(200).send('Message envoyé avec succès');
+            }
+        });
     });
 
     // server.post("/ships", (req, res) => {
